30815cc134c7c4340d2b01108d8a8b9e98dbb3fd
Jing Ai <jingairpi@gmail.com>
2025-02-07 14:48:10 -0800

    Refactor AttentionConfig

--- src/sophia/model/config/attention.py
+++ src/sophia/model/config/attention.py
@@ -1,6 +1,4 @@
-import importlib
-
-from pydantic import field_validator
+from typing import ClassVar, Type
 
 from sophia.model.config.base import BaseConfig
 from sophia.model.layers.bases import AttentionLayer
@@ -15,35 +13,9 @@ class AttentionConfig(BaseConfig):
     to specify different attention implementations.
     """
 
+    expected_base_class: ClassVar[Type[AttentionLayer]] = AttentionLayer
+
     target: str
     hidden_size: int
     num_heads: int
     dropout_rate: float = 0.1
-
-    @field_validator("target", mode="before", check_fields=False)
-    @classmethod
-    def check_target_is_subclass(cls, v: str) -> str:
-        """
-        Validate that the provided 'target' string is a valid fully qualified class name and
-        that the corresponding class is a subclass of `AttentionLayer`.
-        """
-        try:
-            module_name, class_name = v.rsplit(".", 1)
-        except ValueError:
-            raise ValueError(
-                "The target field must be a valid fully qualified class name (module.Class)."
-            )
-
-        module = importlib.import_module(module_name)
-        try:
-            target_class = getattr(module, class_name)
-        except AttributeError:
-            raise ValueError(
-                f"The target class '{v}' does not exist in module '{module_name}'."
-            )
-
-        if not issubclass(target_class, AttentionLayer):
-            raise ValueError(
-                f"The target class '{v}' must be a subclass of `AttentionLayer`."
-            )
-        return v
